f037942ec6af44bdd5726f87d19cadd78d5e5a2b
Rafael Sartori <160548884+RafaelLSartori@users.noreply.github.com>
2024-08-16 12:18:30 -0300

    Atualização

--- src/controller/Ex4.java
+++ src/controller/Ex4.java
@@ -1,2 +1,21 @@
-package controller;public class Ex4 {
+package controller;
+
+public class Ex4 {
+    public Ex4(){
+        super();
+    }
+    public String frecbin(int n){
+        //O ponto de parada foi pensado quando o número digitado pelo usuário chegar a 0.
+        if( n == 0){
+            String s = String.valueOf(0);
+            return s;
+        //Se o número for igual 1, retorna 1.
+        } else if (n == 1){
+            String s = String.valueOf(1);
+            return s;
+        }
+        //A chamada da função recursiva divide o número por 2 e guarda na variável "n" o resto da divisão.
+        //Como a função recursiva vem resolvendo de trás para frente, a ordem resultado vem correta.
+        return frecbin(n / 2 ) + ( n % 2 );
+    }
 }
--- src/view/Main.java
+++ src/view/Main.java
@@ -1,5 +1,17 @@
+package view;
+import javax.swing.JOptionPane;
+import controller.Ex4;
+
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Ex4 m = new Ex4();
+        int num = Integer.parseInt(JOptionPane.showInputDialog("Digite um número para ser convertido: "));
+        while (num > 2000){
+            JOptionPane.showMessageDialog(null,"O número não pode ser maior que 2000!");
+            num = Integer.parseInt(JOptionPane.showInputDialog("Digite um novo número para ser convertido: "));
+        }
+        System.out.println(m.frecbin(num));
     }
 }
